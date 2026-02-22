c68bfdaa2d1cda95383d2de79f8e725e74604f68
Innovation in Education <tanmay.kr2004@gmail.com>
2026-02-22 19:26:12 +0000

    change image to https://upload.wikimedia.org/wikipedia/commons/3/3c/Euro

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
