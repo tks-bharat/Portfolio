8362e6e26c43d90d6e9b2b5fb4ead00f3a0c10e7
Innovation in Education <tanmay.kr2004@gmail.com>
2026-02-22 19:21:29 +0000

    https://media.licdn.com/dms/image/v2/D5603AQF-m2YyBH9NCw/profile-display

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'platerecognizer.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
         port: '',
         pathname: '/**',
       },
