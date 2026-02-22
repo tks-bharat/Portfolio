e038663d8106eb8269c127755c493b132f285da7
Innovation in Education <tanmay.kr2004@gmail.com>
2026-02-22 18:23:36 +0000

    Make the following changes:
    Find this element:
    
      <div class="absolute

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'platerecognizer.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
